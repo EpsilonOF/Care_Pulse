d91d02e0896d0397a3abca61d3d12c1607171501
decapix <decapixd@gmail.com>
2025-02-15 11:30:21 +0100

    ok

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,8 @@
 import motor.motor_asyncio
 
-# Connexion à MongoDB
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://username:password@host:port/dbname")
-db = client['nom_de_la_base']
+# Connexion à MongoDB en utilisant les identifiants et le nom de l'hôte spécifiés dans docker-compose.yml
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://doctogreed:[1[8m.b68Ac~Ke5I{{Sa@mongo:27017/")
+db = client['mgdb-infallible-raman']
 
 async def get_n_records(n):
     """Récupérer n enregistrements de la collection spécifiée."""
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -8,3 +8,8 @@ async def read_data(n: int = 10):
     """Lire les n premiers enregistrements de la base de données."""
     data = await get_n_records(n)
     return {"data": data}
+
+@router.get("/home")
+async def read_data(n: int = 10):
+    """ok"""
+    return {"ok": "ok"}
