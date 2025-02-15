fe213d9029e4b7d832e4e74cdbfdb7893ecc365a
Jean-Baptiste Lépany <jbl@MacBook-Air-de-Jean-Baptiste.local>
2025-02-15 15:27:38 +0100

    good communication for ai translation

--- backend/ds.py
+++ backend/ds.py
@@ -1,15 +1,17 @@
 from openai import OpenAI
+import json
+from io import StringIO
 
 client = OpenAI(
     base_url = "https://api.scaleway.ai/c98de2b2-feb7-4780-a578-4c5276194bf4/v1",
-	api_key = "8b23f64f-b2e5-453d-a23f-3fcf56dfd598"
+	api_key = "a1a533f0-283e-4977-b763-2759d25e1f7f"
 )
 
 def askQuestion(question, language, gender):
     response = client.chat.completions.create(
         model="mistral-nemo-instruct-2407",
         messages=[
-            { "role": "system", "content": "You are a useful AI assistant. You will ask me back the question I am giving it to you. Give me the questions in the correct language, with the proper gender if needed." + "Do it in " + language + ". You are talking to a " + gender },
+            { "role": "system", "content": "You are a useful AI assistant. You will ask me back the question I am giving it to you." + "Ask it in " + language + ". You are talking to a " + gender },
             { "role": "user", "content": question },
         ],
         max_tokens=512,
@@ -20,14 +22,15 @@ def askQuestion(question, language, gender):
     )
     return (response)
 
-def printResponse(response):
+def receiveQuestion(response):
+    output = StringIO()
     for chunk in response:
         if chunk.choices and chunk.choices[-1].delta.content:
-            print(chunk.choices[-1].delta.content, end="", flush=True)
+            output.write(chunk.choices[-1].delta.content)
+    return output.getvalue()
 
-i = 0;
-question = "ais-je de la fatigue lors d'un effort physique ?"
-language = "english"
-gender = "female"
-response = askQuestion(question, language, gender)
-printResponse(response)
+def take_question(file):
+    for question in file:
+        tmp = askQuestion(question['text'], "english", "female")
+        question['text'] =  receiveQuestion(tmp)
+    return (file)
